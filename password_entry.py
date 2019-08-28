18e4a2fb2e34b25bd638b035aa614f0e0d7a33d3
jermainechong <jermaine.chong@my.jcu.edu.au>
2019-08-28 11:07:51 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,7 @@
 """Jermaine"""
+def main():
+    get_password = input('Please enter your password:')
+    get_password = list(get_password)
+    display_password = '*' * len(get_password)
+    print(display_password)
+main()
